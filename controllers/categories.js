cbbca310e1c4752c029c7bf9dd65ac291584f746
CJLynch01 <cjlynch@byui.edu>
2024-10-26 16:39:42 -0600

    Save

--- controllers/categories.js
+++ controllers/categories.js
@@ -3,7 +3,10 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
   const result = await mongodb.getDb().db('recipes').collection('categories').find();
-  result.toArray().then((lists) => {
+  result.toArray().then((err, lists) => {
+    if (err) {
+      res.status(400).json({ message: err });
+    }
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
@@ -12,7 +15,10 @@ const getAll = async (req, res, next) => {
 const getSingle = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb.getDb().db('recipes').collection('categories').find({ _id: userId });
-  result.toArray().then((lists) => {
+  result.toArray().then((err, lists) => {
+    if (err) {
+      res.status(400).json({ message: err });
+    }
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
   });
