5be37becda8f1b6632c5d530628ee2cc351599cf
CJLynch01 <cjlynch@byui.edu>
2024-10-26 16:36:37 -0600

    Save

---
+++ middleware/validate.js
@@ -0,0 +1,28 @@
+const validator = require('../helpers/validate');
+
+const saveRecipe = (req, res, next) => {
+  const validationRule = {
+    recipeName: 'required|string',
+    recipeDescription: 'required|string',
+    recipePrepTime: 'required|string',
+    recipeCookTime: 'required|string',
+    recipeServingSize: 'required|string',
+    recipeIngredients: 'required|string',
+    recipeInstructions: 'required|string'
+  };
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) {
+      res.status(412).send({
+        success: false,
+        message: 'Validation failed',
+        data: err
+      });
+    } else {
+      next();
+    }
+  });
+};
+
+module.exports = {
+  saveContact
+};
--- routes/categories.js
+++ routes/categories.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const categoriesController = require('../controllers/categories');
+const validation = require('../middleware/validate');
 
 router.get('/', categoriesController.getAll);
 
 router.get('/:id', categoriesController.getSingle);
 
-router.post('/', categoriesController.createNewCategory);
+router.post('/', validation.saveRecipe, categoriesController.createNewCategory);
 
-router.put('/:id', categoriesController.editCategory);
+router.put('/:id', validation.saveRecipe, categoriesController.editCategory);
 
 router.delete('/:id', categoriesController.deleteCategory);
 
